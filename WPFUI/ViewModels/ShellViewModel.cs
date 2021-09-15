8ab9f9dd63c403064c0f0007998ab4fc6eb3a0b2
Nestor Mejia <nestor.mejia98@hotmail.com>
2021-09-15 19:32:59 +0200

    data biding

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -9,5 +9,44 @@ namespace WPFUI.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get 
+            {
+                return _firstName; 
+            }
+            set 
+            { 
+                _firstName = value;
+                NotifyOfPropertyChange(()=>FirstName);
+                NotifyOfPropertyChange(()=>FullName);
+            }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            { 
+                return _lastName;
+            }
+            set 
+            { 
+                _lastName = value;
+                NotifyOfPropertyChange(()=>LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+
+
     }
 }
